872256cd63b80a6150bc55dc2f8ae6bedb0af097
begyyal <begyyal@gmail.com>
2022-03-05 02:13:06 +0900

    bugfix

--- src/begyyal/shogi/processor/ReverseDerivationCalculator.java
+++ src/begyyal/shogi/processor/ReverseDerivationCalculator.java
@@ -1,7 +1,5 @@
 package begyyal.shogi.processor;
 
-import java.util.Comparator;
-
 import begyyal.commons.object.Tree;
 import begyyal.commons.object.collection.XList;
 import begyyal.commons.object.collection.XList.XListGen;
@@ -23,18 +21,15 @@ public class ReverseDerivationCalculator {
 	    .collect(XListGen.collect());
     }
 
-    // return -> 選択結果の末端ツリーノード。無ければnull。
     @SuppressWarnings("unchecked")
     private Tree<ResultRecord> r4selectConclusion(Tree<ResultRecord> tree, boolean isSelf) {
-	// 自分は選択の余地があり、相手の選択は全てカバーしている必要がある
-	// つまり、自分はorかつ相手はandで詰みを再帰的に判断する
 
 	var children = tree.getChildren();
 	if (children.isEmpty())
 	    return tree.getDepth() % 2 == 1 ? tree : null;
 
 	Tree<ResultRecord> result = null;
-	long criterion = 0, criterion2 = 0, temp;
+	int depth = 0;
 
 	for (Tree<ResultRecord> child : children.stream()
 	    .sorted((a, b) -> a.getValue().id - b.getValue().id)
@@ -48,21 +43,7 @@ public class ReverseDerivationCalculator {
 	    } else {
 		if (selected == null)
 		    return null;
-		// 相手方は最短詰み筋の深度がより長い選択をする
-		var tips = child.collectTips();
-		final long minDepth = tips
-		    .stream()
-		    .map(t -> t.getDepth())
-		    .filter(d -> d % 2 == 1)
-		    .sorted(Comparator.naturalOrder())
-		    .findFirst().get();
-		if (criterion == minDepth
-			&& criterion2 > (temp = tips
-			    .stream()
-			    .filter(t -> t.getDepth() == minDepth)
-			    .count())
-			&& (criterion2 = temp) > 0
-			|| criterion < minDepth && (criterion = minDepth) > 0)
+		if (depth < (depth = selected.getDepth()))
 		    result = selected;
 	    }
 	}
