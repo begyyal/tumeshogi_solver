839e68979d629d3371db3c7464b92dcc837faf18
begyyal <begyyal@gmail.com>
2023-04-19 12:32:20 +0900

    tag更新時のツイートにWEBのURLを載せる

--- .github/workflows/sh/tweet_template.sh
+++ .github/workflows/sh/tweet_template.sh
@@ -4,9 +4,10 @@ tag_name=$1
 repo_url=$2 # https
 repos_name="tumeshogi_solver"
 ext="#Java #将棋 #詰将棋" # hashtag etc...
+webUrl="https://begyyal.net/tss"
 
 LF=$'\\n'
-text="${repos_name} updated to ${tag_name}${LF}${repo_url}"
+text="${repos_name} updated to ${tag_name}${LF}${webUrl}${LF}${repo_url}"
 [ -n "$ext" ] && text=${text}${LF}${ext} || :
 
 echo -n "{\"text\":\"${text}\"}"
