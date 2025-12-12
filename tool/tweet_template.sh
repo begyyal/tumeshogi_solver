81d73b1451a905db9cd49077c4e02582fa1324fb
begyyal <begyyal@gmail.com>
2025-12-12 11:10:49 +0900

    modify readme.

--- tool/tweet_template.sh
+++ tool/tweet_template.sh
@@ -6,6 +6,6 @@ name="tumeshogi_solver"
 ext="#将棋 #詰将棋 #Java"
 webUrl="https://begyyal.net/tss"
 LF=$'\\n'
-text="${name}@${tag_name} was published.${LF}${webUrl}${LF}${repo_url}"
+text="${name}@${tag_name} was published.${LF}webapp: ${webUrl}${LF}repos: ${repo_url}"
 [ -n "$ext" ] && text=${text}${LF}${ext} || :
 echo -n "{\"text\":\"${text}\"}"
